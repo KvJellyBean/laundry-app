8001e246d93b567ff47006bd8fe3f63c34897ee3
KvJellyBean <kevinnatan91@gmail.com>
2024-05-29 10:48:31 +0700

    feat: add order table migration

---
+++ database/migrations/2024_05_21_150214_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('service_package_id')->constrained()->onDelete('cascade');
+            $table->foreignId('staff_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->string('status')->default('pending');
+            $table->timestamp('processed_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->decimal('total_price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
