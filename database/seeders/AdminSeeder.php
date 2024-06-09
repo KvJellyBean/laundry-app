2b50ca34b11a7e0be3a8338eb7ef9c96ef6d732c
KvJellyBean <kevinnatan91@gmail.com>
2024-06-10 01:39:49 +0700

    fix: add role for admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
 {
@@ -15,11 +16,13 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
+        $adminRole = Role::where('name', 'admin')->first();
+        
         User::create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
             'password' => Hash::make('12345678'),
-            'role' => 'admin',
+            'role' => $adminRole->name,
             'address' => '123 Admin Street',
             'phone_number' => '1234567890',
         ]);
