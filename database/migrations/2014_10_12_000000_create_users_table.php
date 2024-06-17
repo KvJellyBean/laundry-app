f9ce8ca1c65dc957b473abd1502e39b3413f6648
KvJellyBean <kevinnatan91@gmail.com>
2024-06-17 11:39:49 +0700

    feat: add role to user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user')->after('password');
+            $table->string('role')->after('password');
             $table->string('address')->nullable()->after('role');
             $table->string('phone_number')->nullable()->after('address');
             $table->rememberToken();
