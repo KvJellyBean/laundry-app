efb8e9151df942d5e2c36a121d23a02462edad2c
KvJellyBean <kevinnatan91@gmail.com>
2024-06-17 14:11:04 +0700

    fix: solve issues for password and roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
 
 class User extends Authenticatable 
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
 
     protected $fillable = [
         'name',
@@ -18,6 +19,35 @@ class User extends Authenticatable
         'address',
         'phone_number',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($user) {
+            if (!$user->hasRole('user')) {
+                $user->assignRole('user');
+            }
+        });
+    }
+
+    protected $attributes = [
+        'role' => 'user',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        if (substr($value, 0, 4) !== '$2y$') {
+            $this->attributes['password'] = bcrypt($value);
+        } else {
+            $this->attributes['password'] = $value;
+        }
+    }
+
+    public function setRoleAttribute($value)
+    {
+        $this->attributes['role'] = $value ?? 'user';
+    }
 
     public function orders()
     {
