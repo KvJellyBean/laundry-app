7dfe8f10a34569ef59a7a60eac9da9fa2c743ec1
KvJellyBean <kevinnatan91@gmail.com>
2024-06-23 18:13:08 +0700

    feat: add listener for the read more button

--- public/assets/js/apps.js
+++ public/assets/js/apps.js
@@ -3,73 +3,87 @@ const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
+    navLinks.classList.toggle("open");
 
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute(
+        "class",
+        isOpen ? "ri-close-line" : "ri-menu-line"
+    );
 });
 
 navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
 const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
 };
 
 // header container
 ScrollReveal().reveal(".header__container h1", {
-  ...scrollRevealOption,
+    ...scrollRevealOption,
 });
 
 ScrollReveal().reveal(".header__container .btn", {
-  ...scrollRevealOption,
-  delay: 500,
+    ...scrollRevealOption,
+    delay: 500,
 });
 
 // about container
 ScrollReveal().reveal(".about__item", {
-  ...scrollRevealOption,
-  interval: 500,
+    ...scrollRevealOption,
+    interval: 500,
 });
 
 // contact container
 ScrollReveal().reveal(".contact__image img", {
-  ...scrollRevealOption,
-  origin: "right",
-  interval: 500,
+    ...scrollRevealOption,
+    origin: "right",
+    interval: 500,
 });
 
 ScrollReveal().reveal(".contact__card", {
-  interval: 500,
-  duration: 500,
-  delay: 1000,
+    interval: 500,
+    duration: 500,
+    delay: 1000,
 });
 
 // blog container
 ScrollReveal().reveal(".blog__card", {
-  ...scrollRevealOption,
-  interval: 500,
+    ...scrollRevealOption,
+    interval: 500,
 });
 
 const swiper = new Swiper(".swiper", {
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
 });
 
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
 
-registerBtn.addEventListener('click', () => {
+registerBtn.addEventListener("click", () => {
     container.classList.add("active");
 });
 
-loginBtn.addEventListener('click', () => {
+loginBtn.addEventListener("click", () => {
     container.classList.remove("active");
+});
+
+// Service read more button
+const serviceReadMoreBtns = document.querySelectorAll(
+    ".header__container .btn.btn__secondary"
+);
+serviceReadMoreBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const services = document.getElementById("services");
+        services.scrollIntoView({ behavior: "smooth" });
+    });
 });
