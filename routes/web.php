3f402541d0bff40308c7e7663e5d21f14edbdad0
KvJellyBean <kevinnatan91@gmail.com>
2024-06-25 11:49:42 +0700

    fix: remove unused routing

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\LandingController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', [LandingController::class, 'home'])->name('home');
 Route::get('/about', [LandingController::class, 'about'])->name('about');
 Route::get('/services', [LandingController::class, 'services'])->name('services');
@@ -32,35 +28,11 @@ Route::get('/signup', function () {
     return redirect('/dashboard/register');
 })->name('signup');
 
-Route::get('/get-started', function () {
-    return redirect('/dashboard');
-})->name('get-started');
-
-Route::get('/learn-more', function () {
-    return redirect('/services');
-})->name('learn-more');
-
-Route::get('/cuci', function () {
-    return redirect('/dashboard');
-})->name('cuci');
-
-Route::get('/setrika', function () {
-    return redirect('/dashboard');
-})->name('setrika');
-
-Route::get('/cuci-komplit', function () {
-    return redirect('/dashboard');
-})->name('cuci-komplit');
-
 Route::get('/{record}/invoice', [DownloadInvoiceController::class, 'download'])->name('order.invoice');
 
-// Logout
 Route::post('/logout', function () {
     auth()->logout();
-
     request()->session()->invalidate();
-
     request()->session()->regenerateToken();
-
     return redirect()->route('home');
 })->name('logout');
